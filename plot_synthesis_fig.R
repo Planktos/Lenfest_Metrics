b242f36f39e1c316c59749babe850c7b1cee383e
plantkos <kellylynnrobinson@gmail.com>
2019-02-14 13:22:23 -0600

    initial commit

---
+++ plot_synthesis_fig.R
@@ -0,0 +1,10 @@
+#PURPOSE: Create figure for Lenfest metrics paper
+#AUTHOR: Kelly Robinson
+#DATE CREATED: 14 Feb 2019
+
+#DATE LAST MODIFIED: 14 Feb 2019
+
+#libraries
+library(plyr)
+library(dplyr)
+library(ggplot2)
