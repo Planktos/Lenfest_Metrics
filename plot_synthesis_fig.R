fd1b051bd2bf3ceab299614e3f3e406a51c384a4
plantkos <kellylynnrobinson@gmail.com>
2019-02-14 13:45:10 -0600

    read in files

--- plot_synthesis_fig.R
+++ plot_synthesis_fig.R
@@ -8,3 +8,15 @@
 library(plyr)
 library(dplyr)
 library(ggplot2)
+
+#load files
+load("scenario_outputs/synthesis/GoMex_prod_matrices.Rdata")
+
+ps.files <- list.files("scenario_outputs/GoMex_prod_matrices/", pattern = "ps", full.names = T)
+
+gom <- adply(ps.files, 1, function(x){
+
+  y <- read.csv(ps.files[1], header = T)
+  return(y)
+
+}, .progress = T)
